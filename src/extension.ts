d9be836cea6c7effeb7d848db58ae4f2ca53684b
Edgar Albalate <dev.gardo@gmail.com>
2021-04-01 18:46:04 +0200

    Server starts

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,8 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+import { server, send, getPort } from './server';
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -10,23 +12,36 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "diffstream" is now active!');
 
+	vscode.workspace.onDidChangeTextDocument((change) => {
+		const { document: { fileName }, contentChanges } = change;
+		send("changeTextDocument", { fileName, contentChanges });
+	});
+
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
+
 	let startServerDisposable = vscode.commands
 	.registerCommand('diffstream.startServer', () => {
 		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from diffstream!');
+		const port = getPort();
+		if (!server.address()) {
+			server.listen(()=> {
+				const port = getPort();
+				vscode.window.showInformationMessage(`diffstream server running at port ${port}`);
+			});
+		} else {
+			vscode.window.showInformationMessage(`Server also running at port ${port}`);
+		}
 	});
 
 	let endServerDisposable = vscode.commands
-	.registerCommand('diffstream.startServer', () => {
+	.registerCommand('diffstream.endServer', () => {
 		// The code you place here will be executed every time your command is executed
-
+		server.close(() => {
+			vscode.window.showInformationMessage('Server correctly closed');
+		});
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from diffstream!');
 	});
 
 	context.subscriptions.push(startServerDisposable);
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,40 @@
 import { createServer } from 'http';
+import { Duplex } from 'stream';
+
+const stream = new Duplex();
 
 export const server = createServer((req, res) => {
+  if (req.url === "/stream") {
+    res.writeHead(200, {
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      'Content-Type': 'application/json',
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      'Transfer-Encoding': 'chunked'
+    });
+    res.flushHeaders();
+    res.write("[\n");
+    stream.pipe(res);
+    let count = 0;
+    const interval = setInterval(() => {
+      if (count > 10) { res.end(']'); clearInterval(interval); }
+      count++;
+      res.write('{ "type": "ping" },');
+      res.write('\n');
+    }, 1000);
+    return;
+  }
   res.end(req.url);
 });
+
+export const send = function send(type: string, object: any) {
+  stream.write(JSON.stringify({
+    type,
+    object
+  }, null, 2));
+  stream.write(',\n');
+};
+
+export const getPort = function getPort() {
+  const address = server.address();
+	return typeof address === 'string' ? address : address?.port;
+};
