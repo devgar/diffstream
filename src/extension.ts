a2bb9d729097eb3cfd0af9a2fa4043932f9cf199
Edgar Albalate <dev.gardo@gmail.com>
2021-04-02 05:52:48 +0200

    More events listened and sent to server

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,3 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
 import { server, send, getPort } from './server';
@@ -10,20 +8,32 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "diffstream" is now active!');
+	console.log('[activate] "diffstream" is now active!');
+
+	// WORKSPACE listeners
 
 	vscode.workspace.onDidChangeTextDocument((change) => {
-		const { document: { fileName }, contentChanges } = change;
-		send("changeTextDocument", { fileName, contentChanges });
+		const { document, contentChanges } = change;
+		send("changeTextDocument", { document, contentChanges });
 	});
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
+	// WINDOW listeners
+	
+	vscode.window.onDidChangeActiveTextEditor((textEditor) => {
+		send("changeActiveTextEditor", textEditor);
+	});
+
+	vscode.window.onDidChangeTextEditorVisibleRanges((ranges) => {
+		send("changeTextEditorVisibleRanges", ranges);
+	});
+
+	// Commands had been defined in the package.json file
+	// Now provide the implementation of the commands with registerCommand
 	// The commandId parameter must match the command field in package.json
 
 	let startServerDisposable = vscode.commands
 	.registerCommand('diffstream.startServer', () => {
-		// The code you place here will be executed every time your command is executed
+		// Check if server is enabled and enable it if not
 		const port = getPort();
 		if (!server.address()) {
 			server.listen(()=> {
@@ -37,11 +47,9 @@ export function activate(context: vscode.ExtensionContext) {
 
 	let endServerDisposable = vscode.commands
 	.registerCommand('diffstream.endServer', () => {
-		// The code you place here will be executed every time your command is executed
 		server.close(() => {
 			vscode.window.showInformationMessage('Server correctly closed');
 		});
-		// Display a message box to the user
 	});
 
 	context.subscriptions.push(startServerDisposable);
@@ -49,4 +57,6 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+	server.close();
+}
